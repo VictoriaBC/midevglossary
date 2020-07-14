c60f76258b08b6d5ce23635f3c1dabe98e2bc944
VictoriaBC <victoria.benchivar@gmail.com>
2020-07-14 14:00:48 +0000

    Initial commit

--- midevglossary/env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["MONGO_URI"] = 'mongodb+srv://vpb:ztKQZus3.k9hkMY@myfirstcluster-zzbzp.mongodb.net/task_manager?retryWrites=true&w=majority'
-os.environ["SECRET_KEY"] = "verysecretkey"
